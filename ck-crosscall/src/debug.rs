a24dfa153316fc98f629eb00aaf3e304bbeb2fe6
losfair <zhy20000919@hotmail.com>
2019-11-05 20:01:38 +0800

    ck-crosscall: Remove wait in `log`.

--- ck-crosscall/src/debug.rs
+++ ck-crosscall/src/debug.rs
@@ -6,9 +6,5 @@ pub fn log(text: &str) {
         if crosscall::cc_send_message()(&0u128, 0u64, crosscall::MessageType::DEBUG_PRINT as u32, text.as_ptr(), text.len()) < 0 {
             core::intrinsics::abort();
         }
-
-        if crosscall::wait_for_trivial_result_abort_on_error().code != 0 {
-            core::intrinsics::abort();
-        }
     }
 }
